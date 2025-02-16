4aebbbff3e7be36259d424d8a1ad7f240d6e69f5
Sergey Zhukov <szhukov@aligntech.com>
2025-02-16 09:51:55 +0300

    fix: add weird_algorithm()

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Rust exercises for code interview 
 
 ## Useful links
-Arfan Zubi
-- more code example: https://github.com/3rfaan/rust_algorithms/tree/main
+### Arfan Zubi Rust algos and data structures
+- code: https://github.com/3rfaan/rust_algorithms/tree/main
 - video: https://youtu.be/Z5K08WGEuec?si=Krj1Ki-W5n1je-CH
+
+### Arfan Zubi CSES Problem Set
+- cses: https://cses.fi/problemset/list/
+- code: https://github.com/3rfaan/cses/tree/main/src
+- video: https://youtu.be/gN0knvL-EHY?si=JYPIljagCwzb42Kc
---
+++ src/cses/weird_algorithm.rs
@@ -0,0 +1,31 @@
+// Consider an algorithm that takes as input a positive integer n. 
+// If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. 
+// The algorithm repeats this, until n is one.
+
+pub fn weird_algorithm(input: u64) -> Vec<u64> {
+    let mut res = vec![];
+    let mut n = input;
+    res.push(n);
+    while n > 1 {
+        if n % 2 == 0 { 
+            n /= 2; 
+            res.push(n);
+        }
+        else { 
+            n = n * 3 + 1; 
+            res.push(n);
+        }
+        dbg!(n);
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn weird_algorithm_test() {
+        assert_eq!(weird_algorithm(3), vec![3, 10, 5, 16, 8, 4, 2, 1]);
+    }
+}
